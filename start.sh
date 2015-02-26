3e2b2653b49d7825320768f3fbd74f9c97d1dedd
Sean Li <mail@shang.li>
2015-02-26 12:10:48 -0800

    Slightly updated message.

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ then
   echo "$file found."
 else
   echo "$file not found."
-  echo "Moving default index.html..."
+  echo "Copying default index.html..."
   cp /index.html /website_files/index.html
 fi
 /usr/sbin/nginx
